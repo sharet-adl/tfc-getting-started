45796ac0c552a9a0b8c2f5f14c2ab9946570a01e
sharet-adl <57949742+sharet-adl@users.noreply.github.com>
2025-10-22 11:25:13 +0300

    Comment out provider-token for HCL as loaded directly

--- provider.tf
+++ provider.tf
@@ -6,11 +6,12 @@
 # though: using the remote backend, Terraform will execute remotely in HCP Terraform
 # where your token is already securely stored in your workspace!
 
-variable "provider_token" {
-  type      = string
-  sensitive = true
-}
-
+# Keep commented, as REPO is loaded by HCP in workspace directly
+#variable "provider_token" {
+#  type      = string
+#  sensitive = true
+#}
+#
 provider "fakewebservices" {
-  token = var.provider_token
+#  token = var.provider_token
 }
