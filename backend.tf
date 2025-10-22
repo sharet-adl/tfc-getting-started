8ad77a73adb62203181d64e955ae5366b5ab1370
sharet-adl <57949742+sharet-adl@users.noreply.github.com>
2025-10-22 11:06:58 +0300

    Comment out cloud backend configuration
    Comment out the cloud backend configuration in Terraform.

--- backend.tf
+++ backend.tf
@@ -4,13 +4,13 @@
 # The block below configures Terraform to use the 'remote' backend with HCP Terraform.
 # For more information, see https://www.terraform.io/docs/backends/types/remote.html
 terraform {
-  cloud {
-    organization = "{{ORGANIZATION_NAME}}"
-
-    workspaces {
-      name = "{{WORKSPACE_NAME}}"
-    }
-  }
+#  cloud {
+#    organization = "{{ORGANIZATION_NAME}}"
+#
+#    workspaces {
+#      name = "{{WORKSPACE_NAME}}"
+#    }
+#  }
 
   required_version = ">= 1.1.2"
 }
